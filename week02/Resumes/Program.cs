3846d97b37b250565b6501b8345587d2cf50f508
Nick Granados <internick2017@gmail.com>
2025-03-10 22:03:40 -0300

    Add Job and Resume classes with display functionality

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create jobs
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Create resume
+        Resume myResume = new Resume("Nick Granados");
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Member variables
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public Resume(string name)
+    {
+        _name = name;
+    }
+
+
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}

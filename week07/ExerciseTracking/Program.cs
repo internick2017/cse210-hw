47ec7afc330a250a17ce9165fc1a2dad0bc3b52d
Nick Granados <internick2017@gmail.com>
2025-04-12 10:34:01 -0300

    Add Swimming activity class to ExerciseTracking
    - Implemented Swimming class inheriting from Activity
    - Added constructor to initialize date, minutes, and laps
    - Implemented GetDistance, GetSpeed, and GetPace methods

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking;
+
+public static class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("04 Nov 2022", 45, 15.0),
+            new Swimming("05 Nov 2022", 30, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
